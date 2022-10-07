f9ea6c1b4863a23754ba0ebecfce3ca862022cfb
ronelio.oliveira <ronelio.oliveira@galp.com>
2022-10-06 22:05:51 -0300

    Update GalpArImpl.swift

--- src/ios/GalpArImpl.swift
+++ src/ios/GalpArImpl.swift
@@ -2,18 +2,32 @@
   
   @objc(openObject:)
   func openObject(_ command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-    
-      
-      let storyboard = UIStoryboard(name: "GalpArMain", bundle: nil)
-      let vc = storyboard.instantiateViewController(withIdentifier: "mainView")
-      self.viewController.present(vc, animated: true)
-      
-      self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
+      var pluginResult = CDVPluginResult(
+          status: CDVCommandStatus_OK
+      )
+            
+      if(command.methodName == "openObject") {
+          let storyboard = UIStoryboard(name: "GalpArMain", bundle: nil)
+          let viewController = storyboard.instantiateViewController(withIdentifier: "mainView") as! GalpArViewController
+          let jsonData = command.arguments[0] as! NSDictionary
+          
+          if let resource = jsonData.value(forKey: "resourcePath") as? String {
+              viewController.resource = resource
+              self.viewController.present(viewController, animated: true)
+              self.commandDelegate!.send(
+                pluginResult,
+                callbackId: command.callbackId
+              )
+          }
+      } else {
+          pluginResult = CDVPluginResult(
+              status: CDVCommandStatus_ERROR
+          )
+          self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId
+            
+          )
+      }
   }
 }
