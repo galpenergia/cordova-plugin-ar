dcb578fd03f2d8a40aa9bbb14c0abe769502305d
ronelio.oliveira <ronelio.oliveira@galp.com>
2022-10-10 06:00:14 -0300

    Update GalpArViewController.swift

--- src/ios/GalpArViewController.swift
+++ src/ios/GalpArViewController.swift
@@ -12,22 +12,20 @@ import SceneKit
 class GalpArViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
-    
+    var resource = "";
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! GalpArExperience.loadBox()
         
-        guard let url = Bundle.main.url(forResource: "udz", withExtension: "usdz") else {
+        guard let url = Bundle.main.url(forResource: resource, withExtension: nil) else {
             return
         }
         
         let entity = try? Entity.load(contentsOf: url)
 
-
-///  boxAnchor.addChild(entity!)
-        // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
         arView.scene.anchors.first?.addChild(entity!)
         entity!.availableAnimations.forEach { if #available(iOS 15.0, *) {
